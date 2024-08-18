25d5b9062d95a1b1c30c60bbc74f35861451d27d
Cole Horman <colehorman1@gmail.com>
2024-08-18 13:18:16 -0400

    Add Player_Hover

---
+++ Assets/Scripts/Audio/CharacterAudioController.cs
@@ -0,0 +1,13 @@
+using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    void Awake()
+    {
+        Play("Character_Hover");
+    }
+
+   
+}
