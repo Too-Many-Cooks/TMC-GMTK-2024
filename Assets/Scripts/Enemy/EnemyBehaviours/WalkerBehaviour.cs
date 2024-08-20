2fad6fa43d4de74cba051b11f59b872ea4cc1b08
Veguista <66578267+Veguista@users.noreply.github.com>
2024-08-20 17:48:26 +0200

    Stalker enemy variant added

--- Assets/Scripts/Enemy/EnemyBehaviours/WalkerBehaviour.cs
+++ Assets/Scripts/Enemy/EnemyBehaviours/WalkerBehaviour.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class WalkerBehaviour : EnemyBehaviourBase
 {
-    [SerializeField]
-    float speed = 1f;
+    [SerializeField][Range(0, 10)] protected float walkingSpeed = 1f;
 
     // Update is called once per frame
-    void Update()
+    protected virtual void Update()
     {
-        characterController.Move((-transform.position + playerTransform.position).normalized * speed * Time.deltaTime);
+        Move();
         characterController.transform.LookAt(new Vector3(playerTransform.position.x, transform.position.y, playerTransform.position.z));
+    }
+
+    protected virtual void Move()
+    {
+        characterController.Move((-transform.position + playerTransform.position).normalized * walkingSpeed * Time.deltaTime);
     }
 }
